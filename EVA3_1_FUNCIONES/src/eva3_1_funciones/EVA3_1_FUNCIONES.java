40e959fc6f02a3bcaf003946055f308ea940e7f3
Noble0505 <143568699+Noble0505@users.noreply.github.com>
2023-11-21 21:45:48 -0600

    Add files via upload

---
+++ EVA3_1_FUNCIONES/src/eva3_1_funciones/EVA3_1_FUNCIONES.java
@@ -0,0 +1,14 @@
+package eva3_1_funciones;
+
+public class EVA3_1_FUNCIONES {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //LLAMADA A FUNCION
+        imprimirMensaje("HOLA MUNDO!!!");
+    }
+    
+    public static void imprimirMensaje(String mensaje){
+        System.out.println(mensaje);
+    }   
+}
