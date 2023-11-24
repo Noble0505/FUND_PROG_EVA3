cde1d5a53f48784a74206d217a356a837d2d3abb
Noble0505 <143568699+Noble0505@users.noreply.github.com>
2023-11-23 19:43:47 -0600

    Add files via upload

---
+++ EVA3_3_FACTORIAL/src/eva3_3_factorial/EVA3_3_FACTORIAL.java
@@ -0,0 +1,27 @@
+package eva3_3_factorial;
+public class EVA3_3_FACTORIAL {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //llamada a funcion:
+        System.out.println("El factorial de 5 = " + calcularFactorial(5));
+        System.out.println("el resultado de 5 elevado al cuadrado es = " + calcularPotencia(5, 3));
+    }
+    public static int calcularFactorial(int num){
+        //factorial de 5: 1 x 2 x 3 x 4 x 5 = 120
+        //usar ciclos
+        //regresar el valor calculado
+        int resu = 1;
+        for (int i = 1; i <= num; i++) {
+            resu *= i;
+        }
+      return resu;       
+    }   
+    //crear una funcion que calcule la potencia de un numero entero.
+    public static int calcularPotencia(int base, int exp){
+        int igual = 1;
+        for (int i = 0; i < exp; i++) {
+           igual *= base;
+        }
+        return igual;
+}
+}
